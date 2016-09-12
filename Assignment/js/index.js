f62929dfd2e64e5080b9523bde3355f4b77a29ae
Georgie Brown <georginaabrown@gmail.com>
2016-09-12 19:37:35 +1000

    read less button

--- Assignment/js/index.js
+++ Assignment/js/index.js
@@ -1,12 +1,13 @@
 $(document).ready(start);
 
 function start() {
-  alert("hello")
   $('#showContent').click(showParagraph);
 }
 
 function showParagraph(event) {
     event.preventDefault();
     $('#showThisOnClick').show();
+    $('#showContent').hide();
+    $('#readLess').show();
 
 }
